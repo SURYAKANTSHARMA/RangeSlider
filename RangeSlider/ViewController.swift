bdf1dfdd07744bf483d635e7a13d8cee64e4be35
IOS Developer <iosdeveloper@ioss-mac-mini-2.local>
2018-05-09 16:12:06 +0530

    Major bug fixed at update thumb position on changing of new position.

--- RangeSlider/ViewController.swift
+++ RangeSlider/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let rangeSlider = RangeSlider(frame: CGRect.zero)
+    let minValue: Double = 0
+    let maxValue: Double = 1000
+    @IBOutlet weak var lowerValueLabel: UILabel!
+    @IBOutlet weak var upperValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lowerValueLabel.text = "\(minValue)"
+        upperValueLabel.text = "\(maxValue)"
+        rangeSlider.minimumValue = minValue
+        rangeSlider.maximumValue = maxValue
+        rangeSlider.lowerValue = 0.0
+        view.addSubview(rangeSlider)
+        rangeSlider.addTarget(self, action: #selector(sliderValueChange(_:)), for: .valueChanged)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        let margin: CGFloat = 20.0
+        let width = view.bounds.width - 2.0 * margin
+        rangeSlider.frame = CGRect(x: margin, y: margin + view.safeAreaInsets.top + 100,
+                                   width: width, height: 31.0)
+        
     }
-
+    
+    @objc func sliderValueChange(_ sender: RangeSlider) {
+       //print("Range slider value changed: (\(rangeSlider.lowerValue) \(rangeSlider.upperValue))")
+       lowerValueLabel.text = "\(Int(rangeSlider.lowerValue))"
+       upperValueLabel.text = "\(Int(rangeSlider.upperValue))"
+    }
 
 }
+
 
